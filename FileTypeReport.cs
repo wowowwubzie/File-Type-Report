02e8f02cde4b87ad1d384593d8e3372203418914
Deanna S <deannasolis2003@gmail.com>
2025-04-23 15:50:20 -0700

    fixed the kb conversion

--- FileTypeReport.cs
+++ FileTypeReport.cs
@@ -46,10 +46,11 @@ namespace FileTypeReport {
       double size = byteSize;
       int unitIndex = 0;
 
-      while (size>= 1024 && unitIndex < units.Length - 1){
-        size /= 1024;
+      while (size >= 1000 && unitIndex < units.Length - 1) {
+        size /= 1000;
         unitIndex++;
-      }
+        }
+
       return $"{size:0.##} {units[unitIndex]}";
     }
 
